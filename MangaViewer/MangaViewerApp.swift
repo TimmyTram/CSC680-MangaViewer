25c932069656916fd22ccacc94c391cbe38bfb73
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-04-02 11:14:02 -0700

    Initial Commit

---
+++ MangaViewer/MangaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaViewerApp.swift
+//  MangaViewer
+//
+//  Created by Timmy Tram on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
