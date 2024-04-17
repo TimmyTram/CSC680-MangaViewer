1eb1fe38200e9f01041dcb497000b96e6009dfde
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-04-16 19:13:35 -0700

    GameDeal model appears to be functional

--- GameSales/GameSales/Services/GamerPowerService.swift
+++ GameSales/GameSales/Services/GamerPowerService.swift
@@ -4,10 +4,23 @@ import Foundation
 func fetchSales() {
     let url = URL(string: "https://www.gamerpower.com/api/giveaways?platform=steam")!
     
-    
+    let decoder = JSONDecoder()
     let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
         guard let data = data else { return }
-        print(String(data: data, encoding: .utf8)!)
+        //print(String(data: data, encoding: .utf8)!)
+        do {
+            let gameDeals = try decoder.decode([GameDeal].self, from: data)
+            
+            for deal in gameDeals {
+                print("Title: \(deal.title)")
+            }
+            
+        } catch {
+            print("[ERROR]: Failed to decode json: \(error)")
+        }
+        
+        
+        
     }
     
     task.resume()
