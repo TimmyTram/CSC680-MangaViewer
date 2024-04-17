c03bd6423556a8c2b785a68355ff900ddd3b4a64
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-04-16 18:03:09 -0700

    initial state

---
+++ GameSales/GameSales/GameSalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSalesApp.swift
+//  GameSales
+//
+//  Created by Timmy Tram on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GameSalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
